ad1b33dab6634f8c06641d25ccee477915d80ea9
Ventzzz <vicenteherreraaranguiz89@gmail.com>
2024-11-05 22:10:47 -0300

    commit inicial 2

--- index.js
+++ index.js
@@ -17,9 +17,38 @@ const writeData = (data) => {
 
 };
 
-app.get("/getAllLibros", (req, res) => {
-    res.json(readData())
+//POST CREAR CLASE
+app.post("/crearClase", (req, res) => {
+    res.send("CREAR CLASE")
 });
+//GET GENERAR CODIGO
+app.get("/generarCodigoClase/:clase", (req, res) => {
+    res.send("GENERAR CODIGO")
+});
+//POST CONFIRMAR ASISTENCIA
+app.post("/admitirAlumno/:clase/:codigo", (req, res) => {
+    res.send("CONFIRMAR ASISTENCIA")
+});
+//GET OBTENER TODAS LASCLASES
+app.get("/obtenerClases", (req, res) => {
+    res.send("OBTENER TODAS LAS CLASES")
+});
+//GET OBTENER UNA CLASE
+app.get("/obtenerClase/:clase", (req, res) => {
+    res.send("OBTENER UNA CLASE")
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.post("/",(req, res) => {
     res.send("Welcome to my first API with Node Js")
