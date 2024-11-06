af7d10371381aa0285a40963f58b095426c48b0d
Vicente Herrera <127439959+Ventzzz@users.noreply.github.com>
2024-11-06 00:10:15 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 // db.js (archivo de configuración de la base de datos)
-import pkg from 'pg';  // Importa todo el paquete `pg` como un objeto
-const { Pool } = pkg;  // Desestructuramos `Pool` desde el objeto `pkg`
+import pg from 'pg';  // Importa todo el paquete `pg` como un objeto
 
 // Configura el pool de conexiones
-const pool = new Pool({
+const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,  // Usa la URL de la base de datos de Heroku
     ssl: {
         rejectUnauthorized: false  // Habilita SSL en Heroku
